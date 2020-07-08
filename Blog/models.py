928aeab8203da14362ddd26ae5ceca9b526b7b54
enaanfarhan <enaanfo@gmail.com>
2020-07-08 17:44:44 +0600

    Database update.

--- Blog/models.py
+++ Blog/models.py
@@ -11,7 +11,7 @@ class blog(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField()
     date = models.DateField()
-    author = models.CharField(max_length=100, blank=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     image = models.ImageField(blank=True, null=True, upload_to='Blog')
 
     def __str__(self):
