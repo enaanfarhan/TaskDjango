e377a8e5182b088968fbe54098e7b16a303c892f
enaanfarhan <enaanfo@gmail.com>
2020-07-08 16:39:59 +0600

    features update Author list update

--- Blog/urls.py
+++ Blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns=[
     path('list/', blog_list, name="blog-list"),
     path('<int:id>/', blog_post, name="blog-post"),
     path('authors/', author_list, name="authors"),
-    path('authors/<author_name>', author_post),
+    path('authors/<author_name>', author_post, name="author-post"),
 ]
