6184a874dd78ff58c2491f14478fc7fab4935acf
enaanfarhan <enaanfo@gmail.com>
2020-07-14 23:35:42 +0600

    Task 2 update

--- Store/urls.py
+++ Store/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('', product_list, name="store"),
     path('<int:id>/', single_product, name="single-product"),
+    path('category/<category_name>', product_category, name="category-list"),
 ]
--- Store/views.py
+++ Store/views.py
@@ -8,11 +8,15 @@ def product_list(request):
     context = {'pro_list': pro_list}
     return render(request, 'Store/ProductList.html', context)
 
-def category_list(request):
-    cat_list = Category.objects.all()
-    context = {'cat_list':cat_list}
-    return render(request, 'Store/ProductList.html', context)
+
+def product_category(request, category_name):
+    cat_list = Category.objects.get(name=category_name)
+    c_list = product.objects.filter(category=cat_list)
+    context = {'c_list':c_list, 'name':category_name}
+    return render(request, 'Store/ProductCategory.html', context)
+
 
 def single_product(request, id):
     single_prod = product.objects.get(id=id)
     return render(request, 'Store/SingleProduct.html', {'single_prod':single_prod})
+
