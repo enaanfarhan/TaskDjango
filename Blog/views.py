a6f0ed39bbaa6fe39d0ddbaa96d9240fb9a3008e
enaanfarhan <enaanfo@gmail.com>
2020-07-23 20:19:23 +0600

    Bug fix

--- Blog/views.py
+++ Blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
 
@@ -28,13 +28,30 @@ def blog_post(request, slug):
 
 def createblogpost(request):
     form = CreatePostForm()
+    if request.method == 'POST':
+        form = CreatePostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('blog-list')
     context = {
         'form': form
     }
     return render(request, 'Blog/CreateBlogPost.html', context)
 
+#def create_author(request):
+#    form = CreateAuthor()
+#    context = {
+#        'form': form
+#    }
+#   return render(request, 'Blog/AddAuthor.html', context)
+
 def create_author(request):
     form = CreateAuthor()
+    if request.method == 'POST':
+        form = CreateAuthor(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('create-author')
     context = {
         'form': form
     }
